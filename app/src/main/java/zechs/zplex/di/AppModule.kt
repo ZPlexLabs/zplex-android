6a6e38cf65d7eb87126049dc77e08d86c192f4de
zechs <vijay.vky2002@gmail.com>
2022-12-19 20:00:52 +0530

    removed unused `AppModule`

--- app/src/main/java/zechs/zplex/di/AppModule.kt
+++
@@ -1,9 +0,0 @@
-package zechs.zplex.di
-
-import dagger.Module
-import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
-
-@Module
-@InstallIn(SingletonComponent::class)
-object AppModule
