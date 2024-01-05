39d3f6b0e2137da024380414bce8cc04772735a6
zechs <vijay.vky2002@gmail.com>
2024-01-06 01:18:00 +0530

    added a crossfade transition to coil image loader

--- app/src/main/java/zechs/zplex/ThisApp.kt
+++ app/src/main/java/zechs/zplex/ThisApp.kt
@@ -1,7 +1,17 @@
 package zechs.zplex
 
 import android.app.Application
+import coil.ImageLoader
+import coil.ImageLoaderFactory
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class ThisApp : Application()
+class ThisApp : Application(), ImageLoaderFactory {
+
+    override fun newImageLoader(): ImageLoader {
+        return ImageLoader.Builder(this)
+            .crossfade(true)
+            .build()
+    }
+
+}
