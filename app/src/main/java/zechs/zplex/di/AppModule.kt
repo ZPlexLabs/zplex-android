d57b50b94e7f69fe3d0b7c8123036816ab1d0807
zechs <vijay.vky2002@gmail.com>
2024-01-05 22:02:58 +0530

    updated di config

---
+++ app/src/main/java/zechs/zplex/di/AppModule.kt
@@ -0,0 +1,30 @@
+package zechs.zplex.di
+
+import android.content.Context
+import com.google.gson.Gson
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import zechs.zplex.utils.SessionManager
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideGson(): Gson {
+        return Gson()
+    }
+
+    @Singleton
+    @Provides
+    fun provideSessionDataStore(
+        @ApplicationContext appContext: Context,
+        gson: Gson
+    ): SessionManager = SessionManager(appContext, gson)
+
+}
