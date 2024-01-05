f1aec66eff989e7f5b2aedc22bb0b4a217284f91
zechs <vijay.vky2002@gmail.com>
2024-01-06 00:48:54 +0530

    implemented folder picker activity and configured fragment navigations

---
+++ app/src/main/java/zechs/zplex/ui/folder_picker/FolderPickerActivity.kt
@@ -0,0 +1,37 @@
+package zechs.zplex.ui.folder_picker
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import dagger.hilt.android.AndroidEntryPoint
+import zechs.zplex.R
+import zechs.zplex.databinding.ActivityFolderPickerBinding
+
+@AndroidEntryPoint
+class FolderPickerActivity : AppCompatActivity() {
+
+    companion object {
+        const val TAG = "FolderPickerActivity"
+    }
+
+    private lateinit var binding: ActivityFolderPickerBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityFolderPickerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(
+            R.id.folderPickerNavHostFragment
+        ) as NavHostFragment
+        navController = navHostFragment.navController
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+}
