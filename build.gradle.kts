109f1956767e47ed9dc5788c97c92b7e6d74ebfb
zechs <vijay.vky2002@gmail.com>
2024-01-06 15:52:00 +0530

    upgraded agp to 8.2.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.application") version "8.2.0" apply false
+    id("com.android.application") version "8.2.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.21" apply false
     id("com.google.dagger.hilt.android") version "2.48.1" apply false
     id("androidx.navigation.safeargs") version "2.7.4" apply false
