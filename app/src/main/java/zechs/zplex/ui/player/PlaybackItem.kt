62248bde90e930524c0b3a5f6712b56f31482975
zechs <vijay.vky2002@gmail.com>
2024-01-26 12:13:50 +0530

    declared a datamodel for playlist linkedlist

---
+++ app/src/main/java/zechs/zplex/ui/player/PlaybackItem.kt
@@ -0,0 +1,49 @@
+package zechs.zplex.ui.player
+
+
+interface PlaybackItem {
+    val tmdbId: Int
+    val title: String
+    val fileId: String
+    var next: PlaybackItem?
+    var prev: PlaybackItem?
+}
+
+data class Movie(
+    override val tmdbId: Int,
+    override val title: String,
+    override val fileId: String,
+    override var next: PlaybackItem? = null,
+    override var prev: PlaybackItem? = null,
+) : PlaybackItem
+
+
+data class Show(
+    override val tmdbId: Int,
+    override val title: String,
+    override val fileId: String,
+    override var next: PlaybackItem? = null,
+    override var prev: PlaybackItem? = null,
+    val seasonNumber: Int,
+    val episodeNumber: Int,
+    val episodeTitle: String,
+) : PlaybackItem
+
+data class GsonPlaybackItem(
+    val tmdbId: Int,
+    val title: String,
+    val fileId: String,
+    val next: Int?,
+    val prev: Int?,
+    val seasonNumber: Int?,
+    val episodeNumber: Int?,
+    val episodeTitle: String?,
+) {
+    fun toPlaybackItem(): PlaybackItem {
+        return if (seasonNumber != null && episodeNumber != null && episodeTitle != null) {
+            Show(tmdbId, title, fileId, null, null, seasonNumber, episodeNumber, episodeTitle)
+        } else {
+            Movie(tmdbId, title, fileId, null, null)
+        }
+    }
+}
