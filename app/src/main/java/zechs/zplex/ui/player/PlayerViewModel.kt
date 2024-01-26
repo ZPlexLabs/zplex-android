b446cd28f50825a495c7ed0565b523ae3b0359e4
zechs <vijay.vky2002@gmail.com>
2024-01-26 12:14:15 +0530

    implemented playlists logic in playerviewmodel

--- app/src/main/java/zechs/zplex/ui/player/PlayerViewModel.kt
+++ app/src/main/java/zechs/zplex/ui/player/PlayerViewModel.kt
@@ -3,20 +3,30 @@ package zechs.zplex.ui.player
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import zechs.zplex.data.model.entities.WatchedMovie
 import zechs.zplex.data.model.entities.WatchedShow
+import zechs.zplex.data.repository.DriveRepository
 import zechs.zplex.data.repository.WatchedRepository
 import zechs.zplex.ui.player.MPVActivity.Companion.TAG
+import zechs.zplex.utils.SessionManager
+import zechs.zplex.utils.state.Resource
 import java.util.Calendar
 import javax.inject.Inject
 
 @HiltViewModel
 class PlayerViewModel @Inject constructor(
-    private val watchedRepository: WatchedRepository
+    private val watchedRepository: WatchedRepository,
+    private val sessionManager: SessionManager,
+    private val driveRepository: DriveRepository,
+    private val gson: Gson,
 ) : ViewModel() {
 
     private val _startDuration = Channel<Long>(Channel.CONFLATED)
@@ -105,6 +115,63 @@ class PlayerViewModel @Inject constructor(
             watchedDuration, totalDuration, Calendar.getInstance().timeInMillis
         )
         watchedRepository.upsertWatchedMovie(watch)
+    }
+
+
+    data class Playback(
+        val item: PlaybackItem?,
+        val token: String
+    )
+
+    private var head: PlaybackItem? = null
+    private val _current: MutableStateFlow<Resource<Playback?>> = MutableStateFlow(Resource.Loading())
+    val current = _current.asStateFlow()
+
+    fun setPlaylist(playlist: String, startIndex: Int) {
+        val playbackItemType = object : TypeToken<List<GsonPlaybackItem>?>() {}.type
+        gson.fromJson<List<GsonPlaybackItem>>(playlist, playbackItemType)
+            ?.map { it.toPlaybackItem() }
+            ?.let { setPlaylist(it.toList(), startIndex) }
+    }
+
+    private fun setPlaylist(playlist: List<PlaybackItem>, startIndex: Int = 0) {
+        if (playlist.isEmpty()) {
+            head = null
+            updateWithToken()
+        } else {
+            for (i in 0 until playlist.size - 1) {
+                playlist[i].next = playlist[i + 1]
+                playlist[i + 1].prev = playlist[i]
+            }
+            head = playlist[startIndex]
+            updateWithToken()
+        }
+    }
+
+    fun next() {
+        head = head?.next
+        updateWithToken()
+    }
+
+    fun previous() {
+        head = head?.prev
+        updateWithToken()
+    }
+
+    private fun updateWithToken() = viewModelScope.launch(Dispatchers.IO) {
+        val client = sessionManager.fetchClient() ?: run {
+            _current.value = Resource.Error("Client not found")
+            return@launch
+        }
+        when (val tokenResponse = driveRepository.fetchAccessToken(client)) {
+            is Resource.Success -> {
+                _current.value = Resource.Success(Playback(head, tokenResponse.data!!.accessToken))
+            }
+            is Resource.Error -> {
+                _current.value = Resource.Success(Playback(head, tokenResponse.message!!))
+            }
+            else -> {}
+        }
     }
 
 }
