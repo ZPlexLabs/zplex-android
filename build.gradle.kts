fa8182310b5f91600bf0108fdc3eab0e0b7a71de
zechs <vijay.vky2002@gmail.com>
2025-09-21 15:51:39 +0530

    updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    id("com.android.application") version "8.12.2" apply false
+    id("com.android.application") version "8.12.3" apply false
     id("org.jetbrains.kotlin.android") version "2.2.10" apply false
     id("com.google.dagger.hilt.android") version "2.57.1" apply false
-    id("androidx.navigation.safeargs") version "2.9.3" apply false
-    id("com.google.devtools.ksp") version "2.2.10-2.0.2" apply false
+    id("androidx.navigation.safeargs") version "2.9.4" apply false
+    id("com.google.devtools.ksp") version "2.2.20-2.0.3" apply false
 }
