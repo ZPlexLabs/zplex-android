8727672630e0bdcd7204836cfd2c1dbd54307576
zechs <vijay.vky2002@gmail.com>
2026-04-03 19:35:19 +0530

    implement view binding and update viewmodel delegate in homefragment

--- feature-home/src/main/java/zechs/zplex/feature_home/ui/HomeFragment.kt
+++ feature-home/src/main/java/zechs/zplex/feature_home/ui/HomeFragment.kt
@@ -1,31 +1,43 @@
 package zechs.zplex.feature_home.ui
 
-import androidx.fragment.app.viewModels
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import zechs.zplex.feature_home.R
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import zechs.zplex.feature_home.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
     companion object {
-        fun newInstance() = HomeFragment()
+        const val TAG = "HomeFragment"
     }
 
-    private val viewModel: HomeViewModel by viewModels()
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel by activityViewModels<HomeViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // TODO: Use the ViewModel
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
